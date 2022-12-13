5a0c8dbf8207c87f7e7b583472a3905d20b0fbd2
LucyR84 <119548905+LucyR84@users.noreply.github.com>
2022-12-13 13:09:31 -0500

    add zip file and updated code to use zip

--- games_app.py
+++ games_app.py
@@ -13,7 +13,7 @@ import dash_bootstrap_components as dbc
 from dash import dash_table
 
 
-games_data = pd.read_csv("rawg_games_Final.csv")
+games_data = pd.read_csv('rawg_games_Final.zip')
 game_platforms = pd.read_csv('game_platforms_updated.csv')
 game_stores = pd.read_csv('game_stores.csv')
 game_genres = pd.read_csv('game_genres.csv')
